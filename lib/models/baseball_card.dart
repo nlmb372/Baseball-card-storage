980a0b6c8dc759ee32713d77b87b05b51a50c32b
nlmb372 <ilikethisone3456@gmail.com>
2026-02-22 17:53:43 -0600

    Create baseball_card.dart

---
+++ lib/models/baseball_card.dart
@@ -0,0 +1,13 @@
+class BaseballCard {
+  final String playerName;
+  final String year;
+  final String team;
+  final String condition;
+
+  BaseballCard({
+    required this.playerName,
+    required this.year,
+    required this.team,
+    required this.condition,
+  });
+}
